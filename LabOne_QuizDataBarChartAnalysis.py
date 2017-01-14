2f83c635c36175885f09d0d5ae032daf9cb2cc64
EvanGarcia <garciae1@spu.edu>
2017-01-14 13:12:07 -0800

    Update LabOne_QuizDataBarChartAnalysis.py

--- LabOne_QuizDataBarChartAnalysis.py
+++ LabOne_QuizDataBarChartAnalysis.py
@@ -1,7 +1,6 @@
 '''
 This program prompts the user to enter in a quiz score data file, and then reads in the scores and their frequencies.
 It outputs the smallest score, the largest score and the largest frequency, and a score-frequency bar chart and a frequency-score bar chart.
-
 '''
 
 
